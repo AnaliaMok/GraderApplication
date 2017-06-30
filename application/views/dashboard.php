c9b27427db891ca187af09136ce57dd78283ce23
Analia Mok <axm2580@g.rit.edu>
2017-06-30 14:55:03 -0400

    Added table-header styling & added a table-group class

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,7 +1,10 @@
 <main>
-    <div class="table-heading">
-        <h2>Timestamps this Week</h2>
-        <span>Total Time Logged: <?php echo $total_time; ?></span>
+    <div class="table-group">
+        <div class="table-heading">
+            <h2>Timestamps this Week</h2>
+            <span class="outer-span">Total Time Logged: <span><?php echo $total_time; ?></span></span>
+        </div>
+        <?php echo $time_table; ?>
     </div>
-    <?php echo $time_table; ?>
+
 </main>
