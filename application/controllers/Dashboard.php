087b2884e12b1c41418b6f299641bf05d5d36dd5
Analia Mok <axm2580@g.rit.edu>
2017-07-01 16:16:36 -0400

    Applied new structure to Table template. Html attributes are not being applied. May need to structure table manually. [newTable]

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -30,7 +30,65 @@
 
             $this->load->view('templates/header');
             $this->load->view('templates/nav');
-            // TODO: Ask models for data to display
+
+            $data = array();
+            $data = $this->create_time_table($data);
+
+            // TODO: Create unfinished grading table
+            // TODO: Create Finished Grading Table
+
+            // Loading Remaining View & Passing data to dashboard
+            $this->load->view('dashboard', $data);
+            $this->load->view('templates/footer');
+        } // End of view
+
+
+        /**
+         * [create_table_template description]
+         * @return [type] [description]
+         */
+        private function create_table_template(){
+            $template = array(
+                'table_open'            => '<div class="std-table">',
+
+                'thead_open'            => '<div class="table-head">',
+                'thead_close'           => '</div>',
+
+                'heading_row_start'     => '<ul>',
+                'heading_row_end'       => '</ul>',
+                'heading_cell_start'    => '<li>',
+                'heading_cell_end'      => '</li>',
+
+                'tbody_open'            => '<div class="table-body">',
+                'tbody_close'           => '</div>',
+
+                'row_start'             => '<ul>',
+                'row_end'               => '</ul>',
+                'cell_start'            => '<li>',
+                'cell_end'              => '</li>',
+
+                'row_alt_start'         => '<ul>',
+                'row_alt_end'           => '</ul>',
+                'cell_alt_start'        => '<li>',
+                'cell_alt_end'          => '</li>',
+
+                'table_close'           => '</div>'
+            );
+
+            return $template;
+        } // End of create_table_template
+
+
+        /**
+         * create_time_table - Method that asks Timestamps_model for current
+         *      week's timestamps, formats data, and places formatted data
+         *      in data variable
+         *
+         * @param data - Associative array holding all data that will be sent to
+         *               the dashboard view
+         * @return data array
+         */
+        private function create_time_table($data){
 
             // Timestamp data
             $times = $this->Timestamp->get_timestamps();
@@ -38,6 +96,15 @@
 
             // Total Time Logged
             $data['total_time'] = DateTime::createFromFormat('H:i:s', "00:00:00");
+
+            // Empty List Item to place in between inline item groups
+            $empty_cell = array('data' => '&nbsp;', 'class' => 'empty-cell');
+
+            // Creating & Setting Table Template
+            $template = $this->create_table_template();
+            $this->table->set_template($template);
+
+            $table_data = array();
 
             // Creating table rows & formatting date and times
             foreach ($times as $time){
@@ -51,37 +118,54 @@
 
                 $interval = new DateInterval('PT'.$startTime->format('h').'H'.$startTime->format('i').'M');
                 $endTime->sub($interval);
+
                 // End time as an interval
                 $interval = new DateInterval('PT'.$endTime->format('h').'H'.$endTime->format('i').'M');
                 $data['total_time']->add($interval);
 
-                //$data['total_time'] = timespan(mysql_to_unix($time['end']), mysql_to_unix($time['start']), 2);
+                // All cells of a given row
+                $date_cell = array(
+                    'data' => $formattedDate,
+                    'data-title' => 'Date:');
 
-                $this->table->add_row($formattedDate, $formattedStart,
-                    $formattedEnd, $time['name'], $time['section_id']);
+                $start_time_cell = array(
+                    'data' => $formattedStart,
+                    'class' => 'inline-item',
+                    'data-title' => 'Time:');
+
+                $end_time_cell = array(
+                    'data' => $formattedEnd,
+                    'class' => 'inline-item',
+                    'data-title' => '-');
+
+                $name_cell = array(
+                    'data' => $time['name'],
+                    'class' => 'inline-item',
+                    'data-title' => 'Assignment');
+
+                $section_cell = array(
+                    'data' => $time['section_id'],
+                    'class' => 'unbold-title-item inline-item',
+                    'data-title' => 'for');
+
+                // Add cells
+                // $this->table->add_row($date_cell, $empty_cell, $start_time_cell,
+                //     $end_time_cell, $empty_cell, $name_cell, $section_cell);
+                $table_data[] = array($date_cell, $empty_cell, $start_time_cell, $end_time_cell,
+                    $empty_cell, $name_cell, $section_cell);
             }
 
             // Formatting Total Time
             $data['total_time'] = $data['total_time']->format('h'). "hrs ". $data['total_time']->format('i'). "min";
 
-            // Changing the opening table tag for this table
-            $template = array('table_open' => '<table class="std_table">');
-            $this->table->set_template($template);
+            // Placing Generated Table inside data variable
+            $data['time_table'] = $this->table->generate($table_data);
 
-            $data['time_table'] = $this->table->generate();
+            // Clearing table data
+            $this->table->clear();
 
-            // TODO: Create unfinished grading table
-            // TODO: Create Finished Grading Table
-
-            // Loading Remaining View & Passing data to dashboard
-            $this->load->view('dashboard', $data);
-            $this->load->view('templates/footer');
-        } // End of view
-
-
-        public function create_time_table(){
-            // TODO
-        }
+            return $data;
+        } // End of create_time_table
 
 
         public function test(){
--- application/views/test.php
+++ application/views/test.php
@@ -22,20 +22,16 @@
             <div class="table-body">
                 <ul>
                     <li data-title="Date:">03.16.17</li>
+
                     <li class="empty-cell"></li>
-                    <!-- <li class="inline-group">
-                        <ul> -->
+
                     <li data-title="Time:" class="inline-item">8:43 AM</li>
                     <li data-title="-" class="inline-item">10:44 AM</li>
-                        <!-- </ul>
-                    </li> -->
+
                     <li class="empty-cell"></li>
-                    <!-- <li class="inline-group">
-                        <ul> -->
+                    
                     <li data-title="Assignment:" class="inline-item">Project1-1</li>
                     <li data-title="for" class="unbold-title-item inline-item">CSCI-142-08</li>
-                        <!-- </ul>
-                    </li> -->
 
                 </ul><!-- End of last row -->
             </div><!-- End of table-body -->
