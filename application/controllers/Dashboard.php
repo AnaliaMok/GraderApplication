2065d78d02cd978115935be432b1a09412282afb
Analia Mok <axm2580@g.rit.edu>
2017-07-01 17:01:18 -0400

    Generated all html manually. Formatted echoed php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -32,7 +32,7 @@
             $this->load->view('templates/nav');
 
             $data = array();
-            $data = $this->create_time_table($data);
+            $data = $this->create_time_table_two($data);
 
             // TODO: Create unfinished grading table
             // TODO: Create Finished Grading Table
@@ -166,6 +166,83 @@
 
             return $data;
         } // End of create_time_table
+
+
+        private function create_time_table_two($data){
+
+            // Starting with an empty string
+            $data['time_table'] = "";
+
+            // Total Time Logged
+            $data['total_time'] = DateTime::createFromFormat('H:i:s', "00:00:00");
+
+            $startTabs = "\t\t\t\t";
+
+            // Creating table opening
+            $data['time_table'] .= '<div class="std-table">'."\n";
+            $data['time_table'] .= $startTabs.'<div class="table-head">'."\n";
+
+            // Opening ul tag
+            $data['time_table'] .= $startTabs."\t".'<ul>'."\n";
+
+            // Adding Header Items
+            $heading = array("Date", "Start Time", "End Time", "Assignment", "Section");
+
+            foreach ($heading as $head) {
+                $data['time_table'] .= $startTabs."\t\t<li>".$head."</li>\n";
+            }
+
+            // Closing ul & table-head div tag
+            $data['time_table'] .= $startTabs."\t</ul>\n";
+            $data['time_table'] .= $startTabs."</div>\n";
+
+            // Starting table-body div
+            $data['time_table'] .= $startTabs.'<div class="table-body">'."\n";
+
+            // Timestamp Data
+            $times = $this->Timestamp->get_timestamps();
+
+            // Defining an empty-cell
+            $empty_cell = '<li class="empty-cell"></li>'."\n";
+
+            // Creating table rows & formatting date and times
+            foreach ($times as $time){
+                $formattedDate = nice_date($time['date'], 'm.d.y');
+                $formattedStart = date("g:i A", strtotime($time['start']));
+                $formattedEnd = date("g:i A", strtotime($time['end']));
+
+                // TODO: Calculate timespan between start to end & add to total_time var
+                $startTime = DateTime::createFromFormat('H:i:s', $time['start']);
+                $endTime = DateTime::createFromFormat('H:i:s', $time['end']);
+
+                $interval = new DateInterval('PT'.$startTime->format('h').'H'.$startTime->format('i').'M');
+                $endTime->sub($interval);
+
+                // End time as an interval
+                $interval = new DateInterval('PT'.$endTime->format('h').'H'.$endTime->format('i').'M');
+                $data['total_time']->add($interval);
+
+                // Adding New Row
+                $data['time_table'] .= $startTabs."\t<ul>\n";
+                $data['time_table'] .= $startTabs."\t\t".'<li data-title="Date:">'.$formattedDate."</li>\n";
+                $data['time_table'] .= $startTabs."\t\t".$empty_cell."\n";
+                $data['time_table'] .= $startTabs."\t\t".'<li data-title="Time:" class="inline-item">'.$formattedStart."</li>\n";
+                $data['time_table'] .= $startTabs."\t\t".'<li data-title="-" class="inline-item">'.$formattedEnd."</li>\n";
+                $data['time_table'] .= $startTabs."\t\t".$empty_cell."\n";
+                $data['time_table'] .= $startTabs."\t\t".'<li data-title="Assignment" class="inline-item">'.$time['name']."</li>\n";
+                $data['time_table'] .= $startTabs."\t\t".'<li data-title="for" class="unbold-title-item inline-item">'.$time['section_id']."</li>\n";
+                $data['time_table'] .= $startTabs."\t</ul>"."<!-- End of row -->\n";
+            }
+
+            // Closing Tags
+            $data['time_table'] .= $startTabs."</div><!-- End of table-body -->\n";
+            $data['time_table'] .= "\t\t\t</div><!-- End of std-table -->\n";
+
+            // Formatting Total Time
+            $data['total_time'] = $data['total_time']->format('h'). "hrs ". $data['total_time']->format('i'). "min";
+
+            return $data;
+        } // End of create_time_table_two
 
 
         public function test(){
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,11 +1,11 @@
 <main>
     <div class="content">
-    <div class="table-group">
-        <div class="table-heading">
-            <h2>Timestamps this Week</h2>
-            <span class="outer-span">Total Time Logged: <span><?php echo $total_time; ?></span></span>
-        </div>
-        <?php echo $time_table; ?>
-    </div>
-    </div>
+        <div class="table-group">
+            <div class="table-heading">
+                <h2>Timestamps this Week</h2>
+                <span class="outer-span">Total Time Logged: <span><?php echo $total_time; ?></span></span>
+            </div>
+            <?php echo $time_table; ?>
+        </div><!-- End of table group -->
+    </div><!-- End of content -->
 </main>
