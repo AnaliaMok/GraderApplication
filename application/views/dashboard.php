43d0ea680456b56e6e9b2cb89b997aafbfd57540
Analia Mok <axm2580@g.rit.edu>
2017-07-02 10:06:10 -0400

    Added next two table groups. Adjusted js to work with multiple tables

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,5 +1,6 @@
 <main>
     <div class="content">
+        <!-- Time Table -->
         <div class="table-group">
             <div class="table-heading">
                 <h2>Timestamps this Week</h2>
@@ -7,5 +8,23 @@
             </div>
             <?php echo $time_table; ?>
         </div><!-- End of table group -->
+
+        <!-- Unfinished Grading Table -->
+        <div class="table-group">
+            <div class="table-heading">
+                <h2>Unfinished Grading</h2>
+            </div>
+
+        </div><!-- End of table group -->
+
+        <!-- Finished Grading Table -->
+        <div class="table-group">
+            <div class="table-heading">
+                <h2>Finished Grading</h2>
+            </div>
+
+        </div><!-- End of table group -->
+
+
     </div><!-- End of content -->
 </main>
--- assets/js/mainscript.js
+++ assets/js/mainscript.js
@@ -13,48 +13,53 @@ function init(){
     if($(".table-body").length > 0){
         // if table-body exists
         // TODO: Repeat for each table
-        var $table = $(".table-body").get(0);
-        var $rows = $("ul", $table);
+        var tables = $(".table-body");
+        var tableGroups = $('.table-group');
 
-        // Hide Rows 3 and onward
-        if($rows.length > 3){
-            //$rows.slice(3).css("display", "none");
-            var remaining = $rows.slice(3);
-            remaining.toggle();
+        for(var i = 0; i < tables.length; i++){
 
-            // See More Button
-            var button = document.createElement("button");
-            button.appendChild(document.createTextNode("See More"));
+            var currTable = tables.get(i);
+            var currTableGroup = tableGroups.get(i);
+            var rows = $("ul", currTable);
 
-            // Button Styling
-            button.style.fontFamily = "Open Sans, sans-serif";
-            button.style.margin = "0.5em";
-            button.style.backgroundColor = "#EEE";
-            button.style.border = "0";
-            button.style.textDecoration = "underline";
-            button.style.cursor = "pointer";
-            button.style.fontSize = "1em";
-
-            // Adding Toggle Event to Button
-            button.onclick = function(){
+            // Hide Rows 3 and onward
+            if(rows.length > 3){
+                //rows.slice(3).css("display", "none");
+                var remaining = rows.slice(3);
                 remaining.toggle();
-            };
 
-            // Button Holder
-            var newRow = document.createElement("div");
-            newRow.className = "table-button-holder";
-            newRow.appendChild(button);
+                // See More Button
+                var button = document.createElement("button");
+                button.appendChild(document.createTextNode("See More"));
 
-            // Row Styling
-            newRow.style.textAlign = "center";
+                // Button Styling
+                button.style.fontFamily = "Open Sans, sans-serif";
+                button.style.margin = "0.5em";
+                button.style.backgroundColor = "#EEE";
+                button.style.border = "0";
+                button.style.textDecoration = "underline";
+                button.style.cursor = "pointer";
+                button.style.fontSize = "1em";
 
-            // $table.append(newRow);
-            $('.table-group').append(newRow);
+                // Adding Toggle Event to Button
+                button.onclick = function(){
+                    remaining.toggle();
+                };
+
+                // Button Holder
+                var newRow = document.createElement("div");
+                newRow.className = "table-button-holder";
+                newRow.appendChild(button);
+
+                // Row Styling
+                newRow.style.textAlign = "center";
+
+                // $table.append(newRow);
+                currTableGroup.append(newRow);
+
+            }
 
         }
-
-        // Insert See more button after last element
-
 
     }
 
