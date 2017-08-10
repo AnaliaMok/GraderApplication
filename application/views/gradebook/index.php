b0d780d72f31d11e88ea2777d7c62ed8ea9339ca
Analia Mok <axm2580@g.rit.edu>
2017-08-10 13:06:27 -0400

    Edited grade table styling

--- application/views/gradebook/index.php
+++ application/views/gradebook/index.php
@@ -2,20 +2,32 @@
     <div class="content">
 
         <div class="input-section">
-            <!-- Class Section Dropdown -->
-            <?php echo $sections; ?>
             <!-- Button Goes to "Add Student" page-->
             <a href="<?php echo base_url(); ?>gradebook/add_student" class="blue-btn">
                 <span class="ion ion-plus"></span>
                 <span>Add Student(s)</span>
             </a>
-            <!-- TODO: Button Goes to "Add Assignment" page-->
+            <!-- TODO: Button Goes to "Edit Student" page-->
+            <a href="<?php echo base_url(); ?>gradebook/edit_student" class="blue-btn">
+                <span class="ion ion-edit"></span>
+                <span>Edit Student</span>
+            </a>
+            <!-- Button Goes to "Add Assignment" page-->
             <a href="<?php echo base_url(); ?>gradebook/add_assignment" class="blue-btn">
                 <span class="ion ion-plus"></span>
                 <span>Add Assignment(s)</span>
             </a>
+            <!-- TODO: Button Goes to "Edit Assignment" page-->
+            <a href="<?php echo base_url(); ?>gradebook/edit_assignment" class="blue-btn">
+                <span class="ion ion-edit"></span>
+                <span>Edit Assignment</span>
+            </a>
         </div>
         <div class="input-section">
+            <!-- Class Section Dropdown -->
+            <?php echo $sections; ?>
+
+            <!-- Custom Search Bar -->
             <div class="search-bar">
                 <span class="ion ion-android-search"></span>
                 <input type="search" name="search_student" placeholder="Search Students">
@@ -24,7 +36,7 @@
 
         <!-- Grades -->
         <h2 id="selected_section"><?php echo $selected; ?></h2>
-        <div class="table-group" style="margin-top: 1rem;">
+        <div class="table-group" style="margin-top: 1rem; margin-bottom: 2rem;">
             <?php echo $grade_table; ?>
         </div>
 
